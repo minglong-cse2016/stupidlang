527117825e7f2f3b20655c3513dcdcd238f45a3a
minglong-cse2016 <minglong_wu@g.harvard.edu>
2016-09-30 20:06:32 -0400

    stupidlang

--- tests/test_skeleton.py
+++ tests/test_skeleton.py
@@ -8,10 +8,14 @@ __author__ = "minglong-cse2016"
 __copyright__ = "minglong-cse2016"
 __license__ = "mit"
 
+import pytest
+from stupidlang.skeleton import main
 
-def test_fib():
-    assert fib(1) == 1
-    assert fib(2) == 1
-    assert fib(7) == 13
-    with pytest.raises(AssertionError):
-        fib(-10)
+__author__ = "Rahul Dave"
+__copyright__ = "Rahul Dave"
+__license__ = "mit"
+
+
+def test_main():
+    assert main(["tests/test2.sl"])=='7'
+
