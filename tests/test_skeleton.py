fb2a506d47ec411b0d4a331a98d04a77672def22
minglong-cse2016 <minglong_wu@g.harvard.edu>
2016-09-30 22:28:16 -0400

    test

--- tests/test_skeleton.py
+++ tests/test_skeleton.py
@@ -2,19 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-from stupidlang.skeleton import fib
+from stupidlang.skeleton import main
 
 __author__ = "minglong-cse2016"
 __copyright__ = "minglong-cse2016"
 __license__ = "mit"
-
-import pytest
-from stupidlang.skeleton import main
-
-__author__ = "Rahul Dave"
-__copyright__ = "Rahul Dave"
-__license__ = "mit"
-
 
 def test_main():
     assert main(["tests/test2.sl"])=='7'
