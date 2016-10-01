f978790915480211ee9ef8e66d57df8e689920df
minglong-cse2016 <minglong_wu@g.harvard.edu>
2016-09-30 22:33:10 -0400

    test

--- tests/travis_install.sh
+++ tests/travis_install.sh
@@ -28,10 +28,12 @@ if [[ "$DISTRIB" == "conda" ]]; then
     conda create -n testenv --yes python=$PYTHON_VERSION pip
     source activate testenv
 
-elif [[ "$DISTRIB" == "ubuntu" ]]; then
+elif [ [ "$DISTRIB" == "ubuntu" ] ];
+then
     # Use standard ubuntu packages in their default version
 fi
 
-if [[ "$COVERAGE" == "true" ]]; then
+if [ [ "$COVERAGE" == "true" ] ];
+then
     pip install coverage coveralls
 fi
