4b7a2c715dd580474d685fca6f3798e356995888
minglong-cse2016 <minglong_wu@g.harvard.edu>
2016-10-06 12:00:55 -0400

    update

--- stupidlang/skeleton.py
+++ stupidlang/skeleton.py
@@ -30,18 +30,18 @@ __license__ = "mit"
 _logger = logging.getLogger(__name__)
 
 
-def fib(n):
-    """
-    Fibonacci example function
-
-    :param n: integer
-    :return: n-th Fibonacci number
-    """
-    assert n > 0
-    a, b = 1, 1
-    for i in range(n-1):
-        a, b = b, a+b
-    return a
+#def fib(n):
+#    """
+#    Fibonacci example function
+#
+#    :param n: integer
+#    :return: n-th Fibonacci number
+#    """
+#    assert n > 0
+#    a, b = 1, 1
+#    for i in range(n-1):
+#        a, b = b, a+b
+#    return a
 
 
 import os
